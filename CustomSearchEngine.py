7c4501192cbfbd5932263de57cbd6ee11381bba3
itsash04 <160526500+itsash04@users.noreply.github.com>
2025-02-23 16:56:37 +0530

    Update CustomSearchEngine.py

--- CustomSearchEngine.py
+++ CustomSearchEngine.py
@@ -1,11 +1,10 @@
 from exa_py import Exa
-exa= EXA(api_key="d800e108-b97d-45f9-8c1a-11f4228ee303")
+exa= Exa(api_key="d800e108-b97d-45f9-8c1a-11f4228ee303")
 
 searchquery=input('What do you wanna search? ')
 
 response=exa.search(searchquery, num_results=5, type='keyword',include_domains=['https://www.instagram.com'],)
 
 for result in response.results:
-    print(f'Score: {result.score}')
     print(f'Title: {result.title}')
     print(f'URL: {result.url}')
